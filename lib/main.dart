8d37606e80a1616f954b9a0284144695f7dc4133
hackerrank <me@hackerrank.com>
2020-04-18 10:42:59 +0500

    list with map iteration

--- lib/main.dart
+++ lib/main.dart
@@ -2,111 +2,39 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: PremCard(),
+  home: ListData(),
 ));
 
-class PremCard extends StatefulWidget {
+class ListData extends StatefulWidget {
   @override
-  _PremCardState createState() => _PremCardState();
+  _ListDataState createState() => _ListDataState();
 }
 
-class _PremCardState extends State<PremCard> {
+class _ListDataState extends State<ListData> {
 
-  int premLevel = 0;
+  List<String> texts = [
+    'Place there item One',
+    'Place there item Two',
+    'Place there item Three'
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: Text('Prem ID Card'),
-        backgroundColor: Colors.grey[850],
+        backgroundColor: Colors.redAccent,
+        title: Text('Awesome List'),
         centerTitle: true,
-        elevation: 0.0,
+        elevation: 0.0
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            premLevel += 1;
-          });
-        },
-        backgroundColor: Colors.grey[800],
-        child: Icon(Icons.add),
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/pp.jpg'),
-                radius: 40.0,
-              ),
-            ),
-            Divider(
-              height: 90.0,
-              color: Colors.grey,
-            ),
-            Text(
-              'NAME',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(height: 5.0),
-            Text(
-              'Prem Singh',
-              style: TextStyle(
-                  color: Colors.amberAccent[200],
-                  letterSpacing: 2.0,
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Text(
-              'CURRENT PREM LEVEL',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(height: 5.0),
-            Text(
-              '$premLevel',
-              style: TextStyle(
-                  color: Colors.amberAccent[200],
-                  letterSpacing: 2.0,
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.email,
-                  color: Colors.grey[400],
-                ),
-                SizedBox(width: 5.0,),
-                Text(
-                  'premsinghsodha7@gmail.com',
-                  style: TextStyle(
-                      color: Colors.grey[400],
-                      fontSize: 18.0,
-                      letterSpacing: 1.0
-                  ),
-                )
-              ],
-            )
-          ],
-        ),
-      ),
+      body: Column(
+        children: texts.map((t) => Text(t)).toList(),
+      )
     );
   }
 }
+
 
 
 
